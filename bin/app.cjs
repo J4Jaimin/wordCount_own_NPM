85cf2bc8564a7044340148de04270418c47eb62c
Jaimin-1102 <jaimin.rana@agshealth.com-at-205008539362>
2024-12-11 17:12:27 +0530

    add own npm for word count

---
+++ bin/app.cjs
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const fs = require('fs/promises');
+
+// console.log(process.argv);
+
+const file = await fs.readFile(`${process.argv[2]}`, 'utf8');
+
+const obj = {}
+
+for (const line of file.split('\n')) {
+    let regex = /[\W]+/;
+    for (const word of line.toLocaleLowerCase().split(regex).filter((w) => w !== '')) {
+        obj[word] = obj[word] ? obj[word] + 1 : 1
+    }
+}
+
+console.log(obj[process.argv[3]] ? obj[process.argv[3]] : obj);
